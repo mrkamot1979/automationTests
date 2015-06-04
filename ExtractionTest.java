dde902580cc9cc57be2aa9f976da7252abcf27d4
nenu <nino_roldan@yahoo.com>
2015-06-04 18:17:24 +0900

    first commit

---
+++ ExtractionTest.java
@@ -0,0 +1,90 @@
+package tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class ExtractionTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://localhost/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testInvalidUserPwd() throws Exception 
+  {
+   
+	//login  
+	driver.get(baseUrl + "/carinderiaoms/login.php");
+    driver.findElement(By.id("username")).clear();
+    driver.findElement(By.id("username")).sendKeys("nroldan");
+    driver.findElement(By.id("pwd")).clear();
+    driver.findElement(By.id("pwd")).sendKeys("t");
+    driver.findElement(By.name("loginButton")).click();
+    
+    //select the Extract by Date Range from the header
+    driver.findElement(By.linkText("Extract by Date Range")).click();
+    
+    
+
+    
+    
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    //driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
